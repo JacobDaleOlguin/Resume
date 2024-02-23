db19b6b842aa6a7c2cb2e96f80468dec1dc175d9
JacobDaleOlguin <104034339+JacobDaleOlguin@users.noreply.github.com>
2024-02-23 15:05:30 -0500

    overhaul attempt 2

--- src/components/AboutPage.js
+++ src/components/AboutPage.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
 function AboutPage() {
-    return <div>Welcome to the About Page!</div>
+    return <div>Welcome to the About4Page!</div>
 }
 export default AboutPage;
